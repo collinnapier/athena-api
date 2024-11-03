9b0d9691f2f8ccb8004c2e99e5fbabf78e8f3243
collinnapier <collin.napier818@gmail.com>
2024-11-02 23:20:13 -0400

    get patient fix

--- src/patient/getPatient.ts
+++ src/patient/getPatient.ts
@@ -8,13 +8,13 @@ import { FindPatientParams, FindPatientResponse } from "../types";
  * Note: This endpoint may rely on specific settings to be enabled in athenaNet Production to function properly that are not required in other environments. Please see Permissioned Rollout of APIs for more information if you are experiencing issues.
  *
  * @param {FindPatientParams} params - Query Options
- * @returns {Promise<FindPatientResponse>} Athena patient data
+ * @returns {Promise<FindPatientResponse[]>} Athena patient data
  * @throws {AthenaError}
  * @see {@link https://docs.athenahealth.com/api/sandbox#/Chart/getPracticeidChartPatientidAllergies|Athena API Docs}
  */
 export async function getAthenaPatient(
   params: FindPatientParams
-): Promise<FindPatientResponse> {
+): Promise<FindPatientResponse[]> {
   const { patientid, ...query } = params;
   const apiUrl = new URL(
     `${athenaConfig?.origin}/chart/${patientid}/allergies`
@@ -25,7 +25,7 @@ export async function getAthenaPatient(
     headers: { Authorization: athenaConfig?.auth ?? "" },
   });
 
-  const patient: FindPatientResponse = await response.json();
+  const patient: FindPatientResponse[] = await response.json();
 
   await errorCheck(response);
 
